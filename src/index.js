82188e71a07195b41e13d8bc9d65b465bc50d677
Fritz Lekschas <service@lekschas.de>
2018-04-12 11:14:08 -0400

    Add link to live demo

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import mediumZoom from 'medium-zoom';
 import styles from './index.scss';  // eslint-disable-line
 
 mediumZoom('.zoomable', { margin: 48 });
+
+const linkToDemo = document.getElementById('link-to-demo');
+const figTeaser = document.getElementById('fig-teaser');
+
+linkToDemo.addEventListener('mouseenter', () => {
+  figTeaser.setAttribute('class', 'active');
+});
+
+linkToDemo.addEventListener('mouseleave', () => {
+  figTeaser.setAttribute('class', undefined);
+});
